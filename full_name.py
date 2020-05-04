cc62c1f0ada2a2b0207747f295d145ae061277b8
aartikansal <aartilumb@gmail.com>
2020-05-03 21:47:41 -0400

    exercise 5b done

--- full_name.py
+++ full_name.py
@@ -1,5 +1,10 @@
-print ("hello world")
+#print ("hello world")
 
-firstName = "Aarti"
-for x in range (500):
-    print (firstName)
+#firstName = "Aarti"
+#for x in range (500):
+   # print (firstName)
+
+last_Name ="Kansal"
+first_Name= "Aarti"
+full_Name= first_Name + " " + last_Name
+print(full_Name)
